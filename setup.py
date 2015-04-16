2e1931d0c1277d1e4e91e7ec4157413cbbc6c0db
Chris McDonough <chrism@plope.com>
2015-04-16 09:52:44 -0400

    back to dev

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ testing_extras = ['nose', 'coverage']
 docs_extras = ['Sphinx']
 
 setup(name='walkabout',
-      version='0.10',
+      version='0.11.dev0',
       description=('Predicate-based adaptation'),
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
